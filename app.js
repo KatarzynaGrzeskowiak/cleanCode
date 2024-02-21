9c83aed2cef5698794d5888acef6f74f8141aab9
Katarzyna Grześkowiak <tabita33@gmail.com>
2024-02-21 21:02:28 +0100

    fix(extended-1.2): add Media alternatives

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
   deleteButton.className="delete__btn";
   deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.alt = 'remove icon';
   deleteButton.appendChild(deleteButtonImg);
 
 
